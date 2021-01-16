5acb6c904d0078d9d959bd76261c63e0d4b3b4a3
AK0505 <gjajtwmdmwt.59@gmail.com>
2021-01-16 20:59:03 +0900

    修正１　削除機能もbefore_actionにまとめた

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!,  except: [:index, :show]
-  before_action :set_item,  only: [:edit, :show, :update]
+  before_action :set_item,  only: [:edit, :show, :update, :destroy]
 
     def index 
       @items = Item.all.order("created_at DESC")
@@ -38,9 +38,11 @@ class ItemsController < ApplicationController
   end
 
   def destroy 
-    item = Item.find(params[:id])
-    item.destroy
-    redirect_to root_path
+    if @item.destroy
+      redirect_to root_path
+    else
+      render :show
+    end
   end
 
 
