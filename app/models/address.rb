332b7e0b976bd8da8fdce616696025cf51fa5e5e
AK0505 <gjajtwmdmwt.59@gmail.com>
2021-01-18 17:39:49 +0900

    カラムの追加、バリデーションの設定

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,2 +1,15 @@
 class Address < ApplicationRecord
+
+  with_options presence: true do
+    
+    validates :municipality
+    validates :postal_code
+    validates :address
+    validates :phone_number
+    validates :contract_history
+    
+  end
+  
+  validates :building_name
+  validates :prefecture_id,      numericality: { other_than: 0 }
 end
